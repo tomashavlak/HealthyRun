a0cd36f4cb6ef8a2009aefe39c5282ab1915fed9
Tomas Havlak <tomashavlak@seznam.cz>
2017-02-02 14:43:41 +0100

    part of work

--- Assets/Skripty/Butons/StartButton.cs
+++ Assets/Skripty/Butons/StartButton.cs
@@ -3,9 +3,37 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour {
+	bool paused = true;
+	void Start () {
+		paused = true;
+	}
 	public void ChangeScene( string hrSceneName) {
 		SceneManager.LoadScene(hrSceneName, LoadSceneMode.Single);
-		//Application.LoadLevel(hrSceneName);
 		Debug.Log (hrSceneName);
+	}
+
+	public void AddScene(string hrSceneName) {
+		SceneManager.LoadScene(hrSceneName, LoadSceneMode.Additive);
+		Debug.Log (hrSceneName);
+	}
+	public void UnloadScene(string hrSceneName){
+		SceneManager.UnloadScene (hrSceneName);
+	}
+	public void PauseScene() {
+		Debug.Log (paused);
+		paused = !paused;
+
+		if (paused) {
+			Time.timeScale = 1;
+		} else if (!paused) {
+			Time.timeScale = 0;
+		}
+	}
+
+	public void UnpauseScene(string hrPause) {
+		if (hrPause == "true") {
+			Time.timeScale = 1;
+			Debug.Log ("unpaused");
+		}
 	}
 }
