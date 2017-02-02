f280680c5c440c34be384e2ea6b58282ca161ae9
Tomas Havlak <tomashavlak@seznam.cz>
2017-02-02 14:44:22 +0100

    part of work part 2

---
+++ Assets/Skripty/AddScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class AddScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (Screen.width);
+		Debug.Log (Screen.height);
+		SceneManager.LoadScene("MenuGame01", LoadSceneMode.Additive);
+	}
+}
